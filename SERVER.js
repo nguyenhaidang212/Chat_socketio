ac6c9e6435790b3ebceea37e67ee646336ac0cc8
Hai Dang <nguyenhaidanght00@gmail.com>
2022-12-20 19:29:55 +0700

    first commit

--- SERVER.js
+++ SERVER.js
@@ -6,104 +6,88 @@ app.set("views", "./views");
 
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
-server.listen(process.env.PORT || 3000);
-//server.listen(3000);
+//server.listen(process.env.PORT || 3000);
+server.listen(3000);
+
+var mangUsers = [""];
+
+io.on("connection", function(socket) {
+    console.log("Co nguoi ket noi " + socket.id)
+    socket.emit("send-id", socket.id)
+    socket.on("client-send-Username", function(data) {
+        const username = data.username;
+        const id = data.id;
+        const userId = username + "|" + id;
+        if (mangUsers.indexOf(username) >= 0) {
+            socket.emit("server-send-dki-thatbai");
+        } else {
+
+            mangUsers.push(userId);
 
 
-var mangUsers=[""];
+            socket.Username = username;
+            socket.emit("server-send-dki-thanhcong", username);
+            io.sockets.emit("sever-send-danhsach-Users", mangUsers);
+        };
 
-io.on("connection", function(socket){
-  console.log("Co nguoi ket noi " + socket.id)
-  socket.emit("send-id", socket.id)
-  socket.on("client-send-Username", function(data){
-    const username = data.username;
-    const id = data.id;
-    const userId = username + "|"+id;
-    if(mangUsers.indexOf(username)>=0){
-      socket.emit("server-send-dki-thatbai");
-    }else{
+    });
+    var matkhau = new Map();
+    socket.on("send-private-msg", function(data) {
+        let userId = data.user;
+        let msg = data.msg;
+        console.log(userId);
+        let splitUserId = userId.split("|");
+        const username = splitUserId[0];
+        const id = splitUserId[1];
+        if (mangUsers.indexOf(userId) < 0) {
+            socket.emit("send-private-msg-failed");
+        } else {
+            io.to(id).emit("receive-private-msg", data);
+        }
+    })
+    socket.on("tao-room", function(data) {
 
-      mangUsers.push(userId);
+        let room = data.room;
 
+        socket.join(data.room);
+        socket.Phong = data.room;
 
-      socket.Username = username;
-      socket.emit("server-send-dki-thanhcong", username);
-      io.sockets.emit("sever-send-danhsach-Users", mangUsers);
-    };
+        var mang = [];
+        for (const r of socket.adapter.rooms) {
+            if (r[0].length <= 10) {
+                mang.push(r[0]);
+            }
 
-  });
-  var matkhau= new Map();
-  socket.on("send-private-msg", function(data){
-    let userId = data.user;
-    let msg = data.msg;
-    console.log(userId);
-    let splitUserId = userId.split("|");
-    const username = splitUserId[0];
-    const id = splitUserId[1];
-    if(mangUsers.indexOf(userId) <0){
-      socket.emit("send-private-msg-failed");
-    }else{
-      io.to(id).emit("receive-private-msg", data);
-    }
-  })
-  socket.on("tao-room", function(data){
+        }
+        io.sockets.emit("server-send-rooms", mang);
+        socket.emit("server-send-room-socket", data.room);
 
+    });
 
-      let room=data.room;
+    socket.on("user-chat", function(data) {
+        io.sockets.in(socket.Phong).emit("server-chat", { un: data.sender, nd: data.msg });
+    });
 
-      socket.join(data.room);
-      socket.Phong=data.room;
+    socket.on("user-send-message", function(data) {
+        io.sockets.emit("server-send-message", { un: data.sender, nd: data.msg });
 
-
-    var mang=[];
-  for(const r of socket.adapter.rooms){
-    if(r[0].length <= 10){
-      mang.push(r[0]);
-    }
-
-  }
-  io.sockets.emit("server-send-rooms", mang);
-  socket.emit("server-send-room-socket", data.room);
-
-});
-
-  socket.on("user-chat", function(data){
-    io.sockets.in(socket.Phong).emit("server-chat", {un:data.sender, nd:data.msg});
-});
-
-  socket.on("user-send-message", function(data){
-    io.sockets.emit("server-send-message", {un:data.sender, nd:data.msg} );
+    });
 
 
-  });
 
-  socket.on("usertyping", function() {
-    socket.broadcast.emit("typing", "Someone is typing...");
-});
-socket.on("userstoptyping", function() {
-    socket.broadcast.emit("stoptyping");
-});
+    socket.on("user-leave", function(data) {
+        socket.leave(data);
+    });
 
-//socket.on("usertypingroom", function() {
-//  socket.broadcast.emit("typingroom", "Someone is typing...");
-//});
-//socket.on("userstoptypingroom", function() {
-//  socket.broadcast.emit("stoptypingroom");
-//});
-
-  socket.on("user-leave", function(data){
-  socket.leave(data);
-});
-
-socket.on("logout", function(){
-  mangUsers.splice(
-    mangUsers.indexOf(socket.Username), 1
-  );
-  socket.broadcast.emit("sever-send-danhsach-Users", mangUsers);
-});
+    socket.on("logout", function() {
+        mangUsers.splice(
+            mangUsers.indexOf(socket.Username), 1
+        );
+        socket.broadcast.emit("sever-send-danhsach-Users", mangUsers);
+    });
 
 });
 
-app.get("/", function(req, res){
-  res.render("trangchu");
+app.get("/", function(req, res) {
+    res.render("trangchu");
 });
--- public/xuly.js
+++ public/xuly.js
@@ -1,220 +1,234 @@
-var socket = io("https://chat-129144.herokuapp.com");
-var id ="";
-var usernameID ="";
-socket.on("send-id", function(data){
-  id = data;
+const socket = io("http://localhost:3000");
+var id = "";
+var usernameID = "";
+socket.on("send-id", function(data) {
+    id = data;
 })
 
-socket.on("server-send-dki-thatbai", function(){
-  alert("Sai username");
+socket.on("server-send-dki-thatbai", function() {
+    alert("Sai username");
 });
 
-socket.on("sever-send-danhsach-Users", function(data){
-  $("#boxContent").html("");
-  data.forEach(function(i){
-    let splitUserId = i.split("|");
-    const receivedUsername = splitUserId[0];
-    const receivedId = splitUserId[1];
-    if(receivedUsername!='' && receivedId!=''){
-      $("#boxContent").append(`<div class="user">
-        <div class="username">`+receivedUsername+`</div>
-        <div class="userId">ID: `+receivedId+`</div>
+socket.on("sever-send-danhsach-Users", function(data) {
+    $("#boxContent").html("");
+    data.forEach(function(i) {
+        let splitUserId = i.split("|");
+        const receivedUsername = splitUserId[0];
+        const receivedId = splitUserId[1];
+        if (receivedUsername != '' && receivedId != '') {
+            $("#boxContent").append(`<div class="user">
+        <div class="username">` + receivedUsername + `</div>
+        <div class="userId">ID: ` + receivedId + `</div>
       </div>`);
-    }
+        }
 
-  });
-  var myFunction =  function() {
+    });
+    var myFunction = function() {
 
 
         let usernameClick = this.getElementsByClassName("username")[0].innerHTML;
         let userIdClick = this.getElementsByClassName("userId")[0].innerHTML;
 
         let splitUserId = userIdClick.split(": ");
-        const userIdMerged = usernameClick + "|"+splitUserId[1];
+        const userIdMerged = usernameClick + "|" + splitUserId[1];
         $('#txtUserToChat').val(userIdMerged)
 
-};
-  var elements = document.getElementsByClassName("user");
+    };
+    var elements = document.getElementsByClassName("user");
 
-  for(let i = 0; i < elements.length; i++) {
+    for (let i = 0; i < elements.length; i++) {
         elements[i].addEventListener('click', myFunction, false);
     }
 });
 
-socket.on("server-send-dki-thanhcong", function(data){
-  usernameID = data+"|"+id;
-  $("#currentUser").html(data);
-  $("#LoginForm").hide(2000);
-  $("#ChatForm").show(1000);
+socket.on("server-send-dki-thanhcong", function(data) {
+    usernameID = data + "|" + id;
+    $("#currentUser").html(data);
+    $("#LoginForm").hide(2000);
+    $("#ChatForm").show(1000);
 });
 
-socket.on("server-send-rooms", function(data){
-  $("#dsRoom").html("");
-  data.map(function(r){
+socket.on("server-send-rooms", function(data) {
+    $("#dsRoom").html("");
+    data.map(function(r) {
 
-    $("#dsRoom").append("<div class='user'>" + r + "</div>");
-  });
+        $("#dsRoom").append("<div class='user'>" + r + "</div>");
+    });
 });
 
-socket.on("server-send-room-socket", function(data){
-  $("#roomHienTai").html("Chat room: " + data);
+socket.on("server-send-room-socket", function(data) {
+    $("#roomHienTai").html("Chat room: " + data);
 });
 
-var onClickUsername = function(){
-  $('#txtUserToChat').val(this.innerHTML);
+var onClickUsername = function() {
+    $('#txtUserToChat').val(this.innerHTML);
 }
 
-var updateChatClick = function(){
-  var elements = document.getElementsByClassName("usernamechat");
+var updateChatClick = function() {
+    var elements = document.getElementsByClassName("usernamechat");
 
-  for(let i = 0; i < elements.length; i++) {
+    for (let i = 0; i < elements.length; i++) {
         elements[i].addEventListener('click', onClickUsername, false);
     }
 }
 
-socket.on("server-chat", function(data){
-  $("#Group").append("<div class='ms'>" + "<div class='usernamechat'>"+data.un+"</div>" + ": " + data.nd + "</div>");
-  updateChatClick();
-});
-
-socket.on("server-send-message", function(data){
-  $("#ListMessages").append("<div class='ms'>" + "<div class='usernamechat'>"+data.un+"</div>" + ": " + data.nd + "</div>");
-  updateChatClick();
-});
-
-socket.on("stoptyping", function(data) {
-    $("#typingNotify").html("");
-})
-
-socket.on("typing", function(data) {
-    $("#typingNotify").html("<img width='20px' src='typing.gif'>" + data);
-});
-
-socket.on("stoptypingroom", function(data) {
-    $("#typingNotifyroom").html("");
-})
-
-socket.on("typingroom", function(data) {
-    $("#typingNotifyroom").html("<img width='20px' src='typing.gif'>" + data);
-});
-
-socket.on("connect-private-success", function(data){
-  alert("Kết nối thành công");
-  $("#userHientai").html("Đang chat với " + data);
-})
-
-socket.on("connect-private-failed", function(){
-  alert("Kết nối thất bại");
-})
-
-socket.on("send-private-msg-failed", function(){
-  alert("Username hoặc ID không hợp lệ!")
-})
-
-socket.on("receive-private-msg", function(data){
-  let sender=data.sender;
-  $("#userChat").append("<div class='ms'>"+"From "+ "<div class='usernamechat'>"+sender+"</div>" +  ": " + data.msg + "</div>");
-
-  updateChatClick();
-})
-$(document).ready(function(){
-  $("#LoginForm").show();
-  $("#ChatForm").hide();
-
-  $("#txtMessage").focusin(function() {
-    socket.emit("usertyping");
-});
-
-$("#txtMessage").focusout(function() {
-    socket.emit("userstoptyping");
-});
-
-//$("#txtMessageRoom").focusin(function() {
-//  socket.emit("usertypingroom");
-//});
-
-//$("#txtMessageRoom").focusout(function() {
-//  socket.emit("userstoptypingroom");
-//});
-
-  $("#btnRegister").click(function(){
-    let username = $("#txtUsername").val();
-    if (username!= ""){
-      socket.emit("client-send-Username", {username: username, id: id});
-    }
-    else {
-      alert("Username cannot be empty");
+socket.on("server-chat", function(data) {
+    var today = new Date();
+    var time = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    if (data.un == usernameID) {
+        $("#Group").append("<div class='ms toRight backgroundMyMsg' >"  + data.nd + "</div> <br clear='all'/>");
+        $("#Group").append("<div class='msTime toRight'>" + time + "</div> <br clear='all'/>");
+    } else {
+        $("#Group").append("<div class='ms toLeft backgroundTheirMsg'>" + "<div class='usernamechat'>" + data.un + "</div>" + ": " + data.nd + "</div> <br clear='all'/>");
+        $("#Group").append("<div class='msTime toLeft'>" + time + "</div> <br clear='all'/>");
     }
 
-  });
-
-  $("#btnTaoRoom").click(function(){
-    let room=$("#txtRoom").val();
-
-    if(room.length >10){
-      alert("Room name up to 10 characters");
-      $("#txtRoom").val("");
-    }
-    else if(room!=""){
-      socket.emit("tao-room", {
-        room:room
-      });
-      $("#txtRoom").val("");
-
-    }else{
-      alert("Tên hoặc mật khẩu không đúng");
-    }
-    $("#btnTaoRoom").addClass("display-none");
-    $("#btnLeave").removeClass("display-none");
+    updateChatClick();
 });
 
-$("#btnSendMessage").click(function(){
-  let msg = $("#txtMessage").val();
-  if (msg != ""){
-    socket.emit("user-send-message", {msg: msg, sender : usernameID});
-    $("#txtMessage").val("");
-  }
-});
-
-$("#btnSendMessageRoom").click(function(){
-  let msg = $("#txtMessageRoom").val();
-  if(msg != ""){
-    socket.emit("user-chat", {msg: msg, sender : usernameID});
-    $("#txtMessageRoom").val("")
-  }
-
-});
-
-  $("#btnLogout").click(function(){
-    socket.emit("logout");
-    $("#ChatForm").hide(2);
-    $("#LoginForm").show(1);
-    $("#ListMessages").empty();
-    $("#Group").empty();
-  });
-
-
-  $("#btnLeave").click(function(){
-    let roomHtml =  $("#roomHienTai").html();
-    roomHtml = roomHtml.split(": ");
-    roomToSend = roomHtml[1];
-
-    socket.emit("user-leave",roomToSend);
-    $("#roomHienTai").html("Chat room: None");
-    $("#Group").empty();
-    $("#btnTaoRoom").removeClass("display-none");
-    $("#btnLeave").addClass("display-none");
-  });
-
-  $("#btnSendMessageUser").click(function(){
-    let user = $("#txtUserToChat").val();
-    let msg = $("#txtMessageUser").val();
-    if(user != "" && msg!=""){
-      $("#userChat").append("<div class='ms'>" + "To "+  "<div class='usernamechat'>"+user+"</div>" +  ": " + msg + "</div>");
-      socket.emit("send-private-msg", {user: user, msg:msg, sender: usernameID});
-
-      $("#txtMessageUser").val("");
+socket.on("server-send-message", function(data) {
+    var today = new Date();
+    var time = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    if (data.un == usernameID) {
+        $("#ListMessages").append("<div class='ms toRight backgroundMyMsg'>" + data.nd + "</div> <br clear='all'/>");
+        $("#ListMessages").append("<div class='msTime toRight'>" + time + "</div> <br clear='all'/>");
+    } else {
+        $("#ListMessages").append("<div class='ms toLeft backgroundTheirMsg'>" + "<div class='usernamechat'>" + data.un + "</div>" + ": " + data.nd + "</div> <br clear='all'/>");
+        $("#ListMessages").append("<div class='msTime toLeft'>" + time + "</div> <br clear='all'/>");
     }
-  })
+
+    updateChatClick();
+});
+
+
+
+socket.on("connect-private-success", function(data) {
+    alert("Kết nối thành công");
+    $("#userHientai").html("Đang chat với " + data);
+})
+
+socket.on("connect-private-failed", function() {
+    alert("Kết nối thất bại");
+})
+
+socket.on("send-private-msg-failed", function() {
+    alert("Username hoặc ID không hợp lệ!")
+})
+
+socket.on("receive-private-msg", function(data) {
+    let sender = data.sender;
+    var today = new Date();
+    var time = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    $("#userChat").append("<div class='ms toLeft backgroundTheirMsg'>" + "From " + "<div class='usernamechat'>" + sender + "</div>" + ": " + data.msg + "</div> <br clear='all'/>");
+    $("#userChat").append("<div class='msTime toLeft'>" + time + "</div> <br clear='all'/>");
+    updateChatClick();
+})
+$(document).ready(function() {
+    $("#LoginForm").show();
+    $("#ChatForm").hide();
+
+    $("#btnRegister").click(function() {
+        let username = $("#txtUsername").val();
+        if (username != "") {
+            socket.emit("client-send-Username", { username: username, id: id });
+        } else {
+            alert("Username cannot be empty");
+        }
+
+    });
+
+    $("#btnTaoRoom").click(function() {
+        let room = $("#txtRoom").val();
+
+        if (room.length > 10) {
+            alert("Room name up to 10 characters");
+            $("#txtRoom").val("");
+        } else if (room != "") {
+            socket.emit("tao-room", {
+                room: room
+            });
+            $("#txtRoom").val("");
+
+        } else {
+            alert("Tên hoặc mật khẩu không đúng");
+        }
+        $("#btnTaoRoom").addClass("display-none");
+        $("#btnLeave").removeClass("display-none");
+    });
+    const sendMsgGeneral = () => {
+        let msg = $("#txtMessage").val();
+        if (msg != "") {
+            socket.emit("user-send-message", { msg: msg, sender: usernameID });
+            $("#txtMessage").val("");
+        }
+    }
+    $("#btnSendMessage").click(function() {
+        sendMsgGeneral();
+    });
+    $("#txtMessage").keypress(function(event) {
+        if (event.which == 13) {
+            sendMsgGeneral();
+        }
+    });
+    const sendMsgRoom = () => {
+        let msg = $("#txtMessageRoom").val();
+        if (msg != "") {
+            socket.emit("user-chat", { msg: msg, sender: usernameID });
+            $("#txtMessageRoom").val("")
+        }
+
+    }
+    $("#btnSendMessageRoom").click(function() {
+        sendMsgRoom();
+    });
+    $("#txtMessageRoom").keypress(function(event) {
+        if (event.which == 13) {
+            sendMsgRoom();
+        }
+    });
+    $("#btnLogout").click(function() {
+        socket.emit("logout");
+        $("#ChatForm").hide(2);
+        $("#LoginForm").show(1);
+        $("#ListMessages").empty();
+        $("#Group").empty();
+    });
+
+
+    $("#btnLeave").click(function() {
+        let roomHtml = $("#roomHienTai").html();
+        roomHtml = roomHtml.split(": ");
+        roomToSend = roomHtml[1];
+
+        socket.emit("user-leave", roomToSend);
+        $("#roomHienTai").html("Chat room: None");
+        $("#Group").empty();
+        $("#btnTaoRoom").removeClass("display-none");
+        $("#btnLeave").removeClass("display-none");
+    });
+    const sendMsgUser = () => {
+        let user = $("#txtUserToChat").val();
+        let msg = $("#txtMessageUser").val();
+        if (user != "" && msg != "") {
+            var today = new Date();
+            var time = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            $("#userChat").append("<div class='ms toRight backgroundMyMsg'>" + "To " + "<div class='usernamechat'>" + user + "</div>" + ": " + msg + "</div> <br clear='all'/>");
+            $("#userChat").append("<div class='msTime toRight'>" + time + "</div> <br clear='all'/>");
+            socket.emit("send-private-msg", { user: user, msg: msg, sender: usernameID });
+
+            $("#txtMessageUser").val("");
+        }
+    }
+    $("#btnSendMessageUser").click(function() {
+        sendMsgUser();
+    });
+    $('#btnClearMessageUser').click(function() {
+        $("#userChat").html("");
+    });
+    $("#txtMessageUser").keypress(function(event) {
+        if (event.which == 13) {
+            sendMsgUser();
+        }
+    });
 
 });
